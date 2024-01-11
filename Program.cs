b88f8a5074017502cd3ffda5c001a9f3072fc38b
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-11 18:41:42 -0300

    :hammer: Add files tracking

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//This program prompts the user to input a date and then calculates the number of days between that date and today
+
+//To measure the performance of the program
+using System.Diagnostics;
+Stopwatch timeMeasure = new Stopwatch();
+timeMeasure.Start();
+
+String answer = "";
+DateTime today = DateTime.Today;
+
+do {
+    //Ask for a date from the user.
+    Console.WriteLine("Insert the date you want to calculate. Or Write 'exit' to finish.");
+    answer = Console.ReadLine();
+    //If the answer is "break", the program finishes
+    if(answer == "exit"){
+        break;
+    }
+    //Console.WriteLine($"The date introduced was: {answer}");
+
+    //Formating the date introduced:
+    DateTime result;
+    if (DateTime.TryParse(answer, out result)){
+        Console.WriteLine($"The date of today is: {today}");
+
+        if (today == result) {
+            Console.WriteLine($"The day is today!");
+        } else {
+            TimeSpan difference = today - result;
+            switch (today > result){
+                case true: 
+                    
+                    Console.WriteLine($"That date was {difference.Days} days ago");
+                    break;
+                case false:
+                    Console.WriteLine($"That date will be in {-difference.Days} days");
+                    break;
+                
+                    Console.WriteLine($"The day is today!");
+                    break;
+            }
+        }
+
+    }
+    else {
+        Console.WriteLine($"Don't valid date '{answer}', please try again");
+    }
+
+    //Calculating the difference between the day introduced and the day of today:
+
+}while (answer != "exit");
+
+timeMeasure.Stop();
+Console.WriteLine($"Time elapsed: {timeMeasure.Elapsed.TotalMilliseconds/1000} seconds");
+
